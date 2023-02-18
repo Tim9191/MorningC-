5f555ba45e763a6ceaabf2034984f2087db8fc12
Teymur <teymur_aliyev@hotmail.com>
2023-02-18 21:30:58 +0400

    Homework2 final file added

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -14,3 +14,53 @@ int num = Convert.ToInt32(Console.ReadLine());
 int SecondNumber = ShowSecondNumber(num);
 Console.WriteLine($"SecondNumber of {num} is {SecondNumber}");
 */
+
+
+
+// Task 15. Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+bool weekEnd(int dayNum)
+{
+    return (dayNum == 6 || dayNum == 7);
+}
+Console.Write("Input the day number: ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+
+if(weekEnd(dayNum))
+Console.WriteLine("It is a weekend");
+else
+Console.WriteLine("It is a not weekend");
+*/
+
+// Task 13. Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*
+int ThirdDigit(int num)
+{
+    while(num > 999)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+}
+
+Console.WriteLine("Input an integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 100)
+{
+    Console.WriteLine("No third digit");
+}
+else 
+{
+    Console.WriteLine(ThirdDigit(num));
+}
+*/
+
+
+
+    
