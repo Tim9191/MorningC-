243ea090b9738f1a9a12cf158e47f96528156eb5
Teymur <teymur_aliyev@hotmail.com>
2023-03-03 17:27:40 +0400

    Homework6

---
+++ Homework6/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
+int PositiveNum(int m)
+{
+    int temp = 0;
+    for (int i = 0; i < m; i++)
+     {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if(num > 0) temp++;
+     }
+    return temp;
+}
+
+Console.Write("Enter the required numbers: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+while(m < 0)
+{
+    Console.Write($"{m} is not a positive number");
+    m = Convert.ToInt32(Console.ReadLine());
+}
+
+int temp = PositiveNum(m);
+Console.WriteLine($"There are {temp} positive numbers");
+*/
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+/*
+void CrossingOfStraightLine(double k1, double b1, double k2, double b2)
+{
+    if(k1 == k2)
+        if(b1 == b2)
+            Console.WriteLine("The lines match");
+        else
+            Console.WriteLine("The lines are parallel");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Point of intersection of lines is [{x}; {y}]");
+    }
+
+
+}
+
+Console.Write("Enter the value of k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the value of b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the value of k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the value of b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+CrossingOfStraightLine(k1, b1, k2, b2);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Seminar6/Program.cs
@@ -0,0 +1,136 @@
+﻿// Task 1. Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, 
+// а первый - на последнем и т.д.)
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0, j = array.Length - 1; i < j; i++, j--)
+    {
+        int current = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+} 
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+Console.Write("Enter size of array ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter min num in array ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter max num in array ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] newArray = CreateRandomArray(number, min, max);
+ShowArray(newArray);
+ReverseArray(newArray);
+ShowArray(newArray);
+
+
+// Task 2. Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+bool TriangleCheck(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < a + b;
+}
+Console.WriteLine("Enter sides of the triangle");
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (TriangleCheck(a, b, c))
+    Console.Write("Triangle exist");
+else 
+    Console.Write("Triangle does not exist");
+
+
+// Task 3. Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: a и b.
+
+int[] Fibonacci(int size, int a, int b)
+{
+    int[] array = new int[size];
+    array[0] = a;
+    array[1] = b;
+    for (int i = 2; i < size; i++)
+        array[i] = array[i - 1] + array[i - 2];
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+Console.Write("Enter amount ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter first Fibbonachi num ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second Fibbonachi num ");
+int b = Convert.ToInt32(Console.ReadLine());
+PrintArray(Fibonacci(size, a, b));
+
+
+// Task 4. Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] ArrayCopy(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+        newArray[i] = array[i];
+    return newArray;
+}
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+Console.Write("Enter size of array ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter min num in array ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter max num in array ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] newArray = CreateRandomArray(number, min, max);
+PrintArray(newArray);
+Console.WriteLine();
+PrintArray(ArrayCopy(newArray));
+
+
+// Task 5. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+string ConvertToBin(int number)
+{
+    string res = String.Empty;
+
+    while (number > 0)
+    {
+        res = number % 2 + res;
+        number = number / 2; 
+    }
+
+    return res;
+}
+
+Console.Write("Input the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ConvertToBin(number));
